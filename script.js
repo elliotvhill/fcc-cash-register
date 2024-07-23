67614099759ef8c2cd1b14257d6a2dc4d900a64b
Elliot <ellthrowaway2@icloud.com>
2024-07-23 15:15:56 -0400

    initial setup, created status object

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 let price = 1.87;
 let cid = [
-  ['PENNY', 1.01],
-  ['NICKEL', 2.05],
-  ['DIME', 3.1],
-  ['QUARTER', 4.25],
-  ['ONE', 90],
-  ['FIVE', 55],
-  ['TEN', 20],
-  ['TWENTY', 60],
-  ['ONE HUNDRED', 100]
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
 ];
 
 const customerCash = document.getElementById("cash");
 const purchaseBtn = document.getElementById("purchase-btn");
 const changeDue = document.getElementById("change-due");
+const statusMsg = {
+    noFunds: "INSUFFICIENT_FUNDS",
+    closed: "CLOSED",
+    open: "OPEN",
+};
+
+const registerStatus = (status) => {
+    if (statusMsg[status]) {
+        changeDue.textContent = `Status: ${statusMsg[status]}`;
+    }
+};
 
 const makeChange = (cash) => {
     console.log("making change");
@@ -25,6 +36,7 @@ const resetRegister = () => {
 };
 
 purchaseBtn.addEventListener("click", () => {
-    console.log("purchase button clicked")
+    console.log("purchase button clicked");
     makeChange();
+    registerStatus("open");
 });
